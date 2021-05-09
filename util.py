4cce369ed9ad624237906cf5628646bf2bba01bb
Piyush Tank <piyush13290@gmail.com>
2021-05-09 13:53:42 -0500

    started with ch 2, california housing price example

--- util.py
+++ util.py
@@ -24,3 +24,46 @@ def prepare_country_stats(oecd_bli, gdp_per_capita):
     remove_indices = [0, 1, 6, 8, 33, 34, 35]
     keep_indices = list(set(range(36)) - set(remove_indices))
     return full_country_stats[["GDP per capita", 'Life satisfaction']].iloc[keep_indices]
+
+
+
+# for 02_Linear_regression_housing_data, fetching data
+
+def download_housing_data():
+
+    import os
+    import tarfile
+    import urllib.request
+
+    DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+    HOUSING_PATH = os.path.join("datasets", "housing")
+    HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+    def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+        if not os.path.isdir(housing_path):
+            os.makedirs(housing_path)
+        tgz_path = os.path.join(housing_path, "housing.tgz")
+        urllib.request.urlretrieve(housing_url, tgz_path)
+        housing_tgz = tarfile.open(tgz_path)
+        housing_tgz.extractall(path=housing_path)
+        housing_tgz.close()
+        print("California Housing data downloaded")
+
+    return fetch_housing_data()
+
+# for 02, loading the housing data
+
+def get_housing_data():
+    import os
+    import tarfile
+    import urllib.request
+
+    DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+    HOUSING_PATH = os.path.join("datasets", "housing")
+    HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+    def load_housing_data(housing_path=HOUSING_PATH):
+        csv_path = os.path.join(housing_path, "housing.csv")
+        return pd.read_csv(csv_path)
+
+    return load_housing_data()
